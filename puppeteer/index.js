11efaefa5023b4ca6ce4e1d3c26fedef386fb6db
Andrii Iavorskii <aiavorskii@devengineering.com>
2020-10-10 14:43:18 +0300

    Refactoring

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -20,36 +20,41 @@ http.createServer(function (req, res) {
 
         const page = await browser.newPage();
 
+        const wpUrl = 'http://localhost:86';
+        const setLogin = el => el.value = 'test';
+        const setPass = el => el.value = 'q1w2e3r4';
+
         // make a screenshot before
-        await page.goto('http://localhost:86', {waitUntil: 'networkidle2'});
+        await page.goto(wpUrl, {waitUntil: 'networkidle2'});
         await page.screenshot({path: './theme-before.png'});
 
-        await page.goto('http://localhost:86/wp-admin/', {waitUntil: 'networkidle2'});
-
         // login
-        await page.$eval('#user_login', el => el.value = 'test');
-        await page.$eval('#user_pass', el => el.value = 'q1w2e3r4');
+        await page.goto(`${wpUrl}/wp-admin/`, {waitUntil: 'networkidle2'});
+        await page.$eval('#user_login', setLogin);
+        await page.$eval('#user_pass', setPass);
         await Promise.all([
             page.click("#wp-submit"),
             page.waitForNavigation({ waitUntil: 'networkidle0' }),
         ]);
 
         // changing theme
-        await page.goto('http://localhost:86/wp-admin/themes.php', {waitUntil: 'networkidle2'});
+        await page.goto(`${wpUrl}/wp-admin/themes.php`, {waitUntil: 'networkidle2'});
         await Promise.all([
             page.click(".theme:nth-child(3) .button.activate"),
             page.waitForNavigation({ waitUntil: 'networkidle0' }),
         ]);
 
         // make a screenshot after
-        await page.goto('http://localhost:86', {waitUntil: 'networkidle2'});
+        await page.goto(wpUrl, {waitUntil: 'networkidle2'});
         await page.screenshot({path: './theme-after.png'});
         await page.close();
 
         await browser.close();
+
+        res.end("Theme changed");
     })();
 
-    res.end("Theme changed");
+
 }).listen(8009);
 
 
